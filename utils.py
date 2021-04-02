fbac13d646849af9f23b05529b75ba36e4e7b4d5
buuvo95 <buuvo95@gmail.com>
2021-04-02 21:51:55 +0700

    Push on 0402

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import random
 from shutil import copyfile
 import tensorflow as tf
 from matplotlib import pyplot as plt
+import shutil
 
 # Data processing
 def data_processing(train_dir, test_dir, target_size, batch_size, is_binary = False, is_augmentation = True)
@@ -52,16 +53,30 @@ def data_processing(train_dir, test_dir, target_size, batch_size, is_binary = Fa
     return train_generator, validation_generation
 
 # Make training set and test set from dataset
-def make_dataset(dataset_dir, train_dir, test_dir, ratio):
+def make_dataset(dataset_path, train_path, test_path, ratio):
+    """
+    Take dataset path, training path, testing path and ratio
+    to separate image from dataset path to train and test sets
+    """
     # Take lenght of files in dataset_dir then multiple by ratio
     # Then random pick to the lenght of ratio and copy to the train set
-    file_len = len([name for name in os.listdir(dataset_dir) if os.path.isfile(os.path.join(dataset_dir,name))])
-
-    train_len = int(ratio * file_len)
-    test_len = file_len - train_len
-
-    #Random pick an image and move to the train_dataset
-
+    train = list()
+    test = list()
+    dataset_list = os.listdir(dataset_path)
+    train = random.sample(dataset_list, int(ratio*len(dataset_list)))
+    for xfile in os.listdir(dataset_path):
+        if xfile not in train:
+            test.append(xfile)
+    #Copy filename to the train_dataset
+    for fname in train:
+        src = os.path.join(dataset_path, fname)
+        dst = os.path.join(train_path, fname)
+        shutil.copyfile(src, dst)
+    for fname in test:
+        src = os.path.join(dataset_path, fname)
+        dst = os.path.join(test_path, fname)
+        shutil.copyfile(src, dst)
+        
 # Plot the training process
 def plot(history):
     acc = history.history['acc']
