a2ded69e2e605a1ea23f26a58b9d4979ec32c385
buuvo95 <buuvo95@gmail.com>
2021-04-10 22:35:03 +0700

    add Data augmentation

---
+++ datagen.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+class trainDatagen():
+    def __init__(self, 
+                 width_shift_range,
+                 height_shift_range,
+                 rotation_range,
+                 zoom_range,
+                 shear_range,
+                 horizontal_flip,
+                 vertical_flip):
+        self. train_datagen = ImageDataGenerator(
+            rescale = 1./255,
+            width_shift_range = width_shift_range,
+            height_shift_range = height_shift_range,
+            rotation_range = rotation_range,
+            zoom_range = zoom_range,
+            shear_range = shear_range,
+            horizontal_flip = horizontal_flip
+            vertical_flip = vertical_flip
+            )
+    def generator(self, train_dir, target_size, batch_size, class_mode):
+        self.train_generator = self.train_datagen(
+            train_dir,
+            target_size = target_size,
+            batch_size = batch_size,
+            class_mode = class_mode
+        )
+        return train_generator
+
+class testDatagen():
+    def __init__(self):
+        self.test_datagen = ImageDataGenerator(rescale = 1./255)
+    def generator(self, test_dir, target_size, batch_size, class_mode):
+        self.test_generator = self.test_datagen(
+            test_dir,
+            target_size = target_size,
+            batch_size = batch_size,
+            class_mode = class_mode
+        )
+        return test_generator
--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from datagen import *
 # from model import *
 
 
